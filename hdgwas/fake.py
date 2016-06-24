f8652383d59f21ee46ee2f476a78195030b480d4
gena <gena.roshchupkin@gmail.com>
2016-06-24 19:23:20 +0200

    encode bug fix

--- hdgwas/fake.py
+++ hdgwas/fake.py
@@ -94,7 +94,7 @@ class Encoder(object):
 			os.path.join(save_path,str(self.hdf5_iter)+'_'+self.study_name +'.h5'), 'w', title='encode_genotype')
 		self.hdf5_iter+=1
 
-		atom = tables.Float16Atom() # TODO (mid) check data format, Int8Atom works only for best guess, minimac needs Float16
+		atom = tables.Float32Atom() # TODO (mid) check data format, Int8Atom works only for best guess, minimac needs Float16
 		self.genotype = self.h5_gen_file.createCArray(self.h5_gen_file.root, 'genotype', atom,
 													  (data.shape),
 													  title='Genotype', filters=self.pytable_filters)
