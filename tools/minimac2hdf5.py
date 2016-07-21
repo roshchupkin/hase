2da866987c0f49b2611b1628f69c66a27db2c002
roshchupkin <gena.roshchupkin@gmail.com>
2016-07-21 13:13:48 +0200

    probes converter memory fix

--- tools/minimac2hdf5.py
+++ tools/minimac2hdf5.py
@@ -12,17 +12,79 @@ import numpy as np
 from hdgwas.tools import Timer
 import tables
 
-def probes_minimac2hdf5(data_path, save_path,study_name):
-	n=[]
-	f=open(data_path,'r')
-	for i,j in enumerate(f):
-		n.append((j[:-1]).split(' '))
-	f.close()
-	n=np.array(n)
-	chunk=pd.DataFrame.from_dict({"ID":n[:,0],'allele1':n[:,1],'allele2':n[:,2],'MAF':n[:,3],'Rsq':n[:,4]})
 
-	chunk.to_hdf(os.path.join(save_path,'probes',study_name+'.h5'), key='probes',format='table',
-			                   min_itemsize = 25, complib='zlib',complevel=9 )
+
+_proc_status = '/proc/%d/status' % os.getpid()
+
+_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
+		  'KB': 1024.0, 'MB': 1024.0*1024.0}
+
+def _VmB(VmKey):
+	'''Private.
+    '''
+	global _proc_status, _scale
+	# get pseudo file  /proc/<pid>/status
+	try:
+		t = open(_proc_status)
+		v = t.read()
+		t.close()
+	except:
+		return 0.0  # non-Linux?
+		# get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+	i = v.index(VmKey)
+	v = v[i:].split(None, 3)  # whitespace
+	if len(v) < 3:
+		return 0.0  # invalid format?
+		# convert Vm value to bytes
+	return float(v[1]) * _scale[v[2]]
+
+
+def memory(since=0.0):
+	'''Return memory usage in bytes.
+    '''
+	return _VmB('VmSize:') - since
+
+
+def resident(since=0.0):
+	'''Return resident memory usage in bytes.
+    '''
+	return _VmB('VmRSS:') - since
+
+
+def probes_minimac2hdf5(data_path, save_path,study_name):
+	#raise ValueError('test')
+	print 'memory before read probes', memory()
+	n=[]
+
+
+	df=pd.read_csv(data_path,sep=' ',chunksize=500000, header=None,index_col=None)
+
+	for i,chunk in enumerate(df):
+		print 'add chunk {}'.format(i)
+		chunk.columns=["ID",'allele1','allele2','MAF','Rsq']
+		chunk.to_hdf(os.path.join(save_path,'probes',study_name+'.h5'), key='probes',format='table',append=True,
+				 min_itemsize = 25, complib='zlib',complevel=9 )
+
+	# f=open(data_path,'r')
+	# for i,j in enumerate(f):
+	# 	n.append((j[:-1]).split(' '))
+	# 	if i>=500000 and i%500000==0:
+	# 		print 'add chunk {}'.format(str(i/500000) )
+	# 		n=np.array(n)
+	# 		print n.shape
+	# 		chunk=pd.DataFrame.from_dict({"ID":n[:,0],'allele1':n[:,1],'allele2':n[:,2],'MAF':n[:,3],'Rsq':n[:,4]})
+	# 		n=[]
+	# 		chunk.to_hdf(os.path.join(save_path,'probes',study_name+'.h5'), key='probes',format='table',append=True,
+	# 			 min_itemsize = 25, complib='zlib',complevel=9 )
+    #
+	# f.close()
+	# print 'memory after read probes', memory()
+	# n=np.array(n)
+	# print 'memory after probes2npy', memory()
+	# chunk=pd.DataFrame.from_dict({"ID":n[:,0],'allele1':n[:,1],'allele2':n[:,2],'MAF':n[:,3],'Rsq':n[:,4]})
+	# print 'memory after dic', memory()
+	# chunk.to_hdf(os.path.join(save_path,'probes',study_name+'.h5'), key='probes',format='table',append=True,
+	# 			 min_itemsize = 25, complib='zlib',complevel=9 )
 
 def ind_minimac2hdf5(data_path, save_path,study_name):
 	n=[]
@@ -33,7 +95,7 @@ def ind_minimac2hdf5(data_path, save_path,study_name):
 	n=np.array(n)
 	chunk=pd.DataFrame.from_dict({"individual":n})
 	chunk.to_hdf(os.path.join(save_path,'individuals',study_name+'.h5'), key='individuals',format='table',
-			                   min_itemsize = 25, complib='zlib',complevel=9 )
+				 min_itemsize = 25, complib='zlib',complevel=9 )
 
 def id_minimac2hdf5(data_path,id, save_path):
 
